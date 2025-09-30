5c8ddcd938eae432f17c12fa3298d3f7c2b8a971
Roger Mähler <roger.mahler@hotmail.com>
2025-09-30 19:35:03 +0200

    refactor: clean up imports and streamline startup process in main.py

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-import psycopg
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from configuration.config import Config
-from configuration.inject import ConfigStore, ConfigValue
-from src.api.router import router
-from utility import configure_logging, create_db_uri
+from src.api.router import router, setup_config_store
 
-ConfigStore.configure_context(source="config/config.yml", env_filename=".env", env_prefix="SEAD_AUTHORITY")
-
+# Force import of strategies to register them
+import src.strategies  # pylint: disable=unused-import
 
 app = FastAPI(title="SEAD Entity Reconciliation Service")
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"])
@@ -20,15 +17,15 @@ app.include_router(router)
 async def startup():
     try:
 
-        configure_logging(ConfigValue("logging").resolve() or {})
+        await setup_config_store()
 
-        cfg: Config = ConfigStore.config()
-        dsn: str = create_db_uri(**cfg.get("options:database"))
-        app.state.conn = await psycopg.AsyncConnection.connect(dsn)
-        app.state.config = cfg
+        # configure_logging(cfg.get("logging") or {})
 
-        cfg.add({"runtime:connection": app.state.conn})
+        # dsn: str = create_db_uri(**cfg.get("options:database"))
+        # app.state.conn = await psycopg.AsyncConnection.connect(dsn)
+        # app.state.config = cfg
 
+        # cfg.add({"runtime:connection": app.state.conn})
     except Exception as e:
         print(f"Failed to connect to database: {e}")
         raise
