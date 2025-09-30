4317dd6ae8b63f65765381dbbe5300551aa08b43
Roger Mähler <roger.mahler@hotmail.com>
2025-09-30 09:50:10 +0200

    test: add conftest.py for test configuration and fixtures

---
+++ tests/conftest.py
@@ -0,0 +1,12 @@
+from unittest.mock import AsyncMock
+import psycopg
+import pytest
+from src.configuration.inject import ConfigStore, Config
+
+ConfigStore.configure_context(source="./tests/config.yml", env_filename=None)
+ConfigStore.config().update({"runtime:connection": AsyncMock(spec=psycopg.AsyncConnection)})
+
+@pytest.fixture(scope="session")
+def cfg() -> Config:
+    ConfigStore.configure_context(source="./tests/config.yml", env_filename=None)
+    return ConfigStore.config()
