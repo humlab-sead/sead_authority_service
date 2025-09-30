0ccb41e85e7bad0c91b4b1f414bc4682ffc6ccaf
Roger Mähler <roger.mahler@hotmail.com>
2025-09-30 09:51:25 +0200

    feat: implement reconcile_queries function for handling query reconciliation

---
+++ src/reconcile.py
@@ -0,0 +1,39 @@
+from typing import Any
+
+from psycopg.rows import dict_row
+
+from src.configuration.inject import ConfigValue
+from src.strategies.interface import ReconciliationStrategy, Strategies
+
+async def reconcile_queries(queries: dict[str, Any]) -> dict[str, Any]:
+
+    connection = ConfigValue("runtime:connection").resolve()
+    default_query_limit: int = ConfigValue("options:default_query_limit").resolve() or 10
+
+    results: dict[str, Any] = {}
+    async with connection.cursor(row_factory=dict_row) as cursor:
+        for query_id, query in queries.items():
+
+            if not (query.get("query") or "").strip():
+                results[query_id] = {"result": []}
+                continue
+
+            if not query.get("type"):
+                raise ValueError("Missing 'type' in query")
+
+            entity_type: str = query.get("type")
+
+            if not Strategies.items.get(entity_type):
+                raise ValueError(f"Unknown query type '{entity_type}' in query")
+
+            strategy: ReconciliationStrategy = Strategies.items.get(entity_type)
+
+            candidate_data: list[dict[str, Any]] = await strategy.find_candidates(
+                cursor,
+                query=query.get("query"),
+                properties={p["pid"]: p["v"] for p in query.get("properties", []) if "pid" in p and "v" in p},
+                limit=default_query_limit,
+            )
+
+            results[query_id] = {"result": [strategy.as_candidate(data) for data in candidate_data]}
+        return results
