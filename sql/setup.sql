05a73d89b55e453366c8dadf01c20edb6e47efba
Roger Mähler <roger.mahler@hotmail.com>
2025-10-04 10:30:09 +0200

    refactor: update view and function definitions for locations and feature types

--- sql/setup.sql
+++ sql/setup.sql
@@ -13,7 +13,7 @@ as $$
 $$;
 
 /**********************************************************************************************
-**  Sites
+**  Site
 **********************************************************************************************/
 
 drop view if exists authority.sites;
@@ -57,9 +57,8 @@ as $$
 $$;
 
 /**********************************************************************************************
-**  Locations
+**  Location
 **********************************************************************************************/
-
 
 drop view if exists authority.locations;
 create or replace view authority.locations as
@@ -79,26 +78,85 @@ create index if not exists tbl_locations_norm_trgm
   on public.tbl_locations
     using gin ( (authority.immutable_unaccent(lower(location_name))) gin_trgm_ops );
 
-
 drop function if exists authority.fuzzy_locations(text, integer);
-create or replace function authority.fuzzy_locations(p_text text, p_limit integer default 10)
-returns table ( location_id integer, label text, name_sim double precision )
+create or replace function authority.fuzzy_locations(
+	p_text text,
+	p_limit integer default 10,
+	variadic location_type_ids integer[] default null)
+returns table (
+	location_id integer,
+	label text,
+	name_sim double precision
+)
 language sql
 stable
 as $$
-    with params as (
+  with params as (
         select authority.immutable_unaccent(lower(p_text))::text as q
-    )
-        select
-        s.location_id,
-        s.label,
-        greatest(
-            case when s.norm_label = (select q from params) then 1.0
-                else similarity(s.norm_label, (select q from params))
-            end, 0.0001
-        ) as name_sim
-        from authority.locations as s
-        where s.norm_label % (select q from params)       -- trigram candidate filter
-        order by name_sim desc, s.label
-        limit p_limit;
+  ), location_types as (
+		select location_type_id
+		from tbl_location_types
+		where array_length(location_type_ids, 1) is null
+		 or location_type_id = ANY(location_type_ids)
+	)
+    select
+      s.location_id,
+      s.label,
+      greatest(
+          case when s.norm_label = (select q from params) then 1.0
+              else similarity(s.norm_label, (select q from params))
+          end, 0.0001
+      ) as name_sim
+    from authority.locations as s
+    join location_types using (location_type_id)
+    where s.norm_label % (select q from params)       -- trigram candidate filter
+    order by name_sim desc, s.label
+    limit p_limit;
+$$;
+
+
+/**********************************************************************************************
+**  Location
+**********************************************************************************************/
+
+
+drop view if exists authority.feature_types;
+create or replace view authority.feature_types as
+  select  feature_type_id,
+          feature_type_name as label,
+          feature_type_description as description,
+          authority.immutable_unaccent(lower(feature_type_name)) as norm_label
+  from public.tbl_feature_types;
+
+create index if not exists tbl_feature_types_norm_trgm
+  on public.tbl_feature_types
+    using gin ( (authority.immutable_unaccent(lower(feature_type_name))) gin_trgm_ops );
+
+drop function if exists authority.fuzzy_feature_types(text, integer);
+create or replace function authority.fuzzy_feature_types(
+	p_text text,
+	p_limit integer default 10
+) returns table (
+	feature_type_id integer,
+	label text,
+	name_sim double precision
+)
+language sql
+stable
+as $$
+  with params as (
+        select authority.immutable_unaccent(lower(p_text))::text as q
+  )
+    select
+      s.feature_type_id,
+      s.label,
+      greatest(
+          case when s.norm_label = (select q from params) then 1.0
+              else similarity(s.norm_label, (select q from params))
+          end, 0.0001
+      ) as name_sim
+    from authority.feature_types as s
+    where s.norm_label % (select q from params)       -- trigram candidate filter
+    order by name_sim desc, s.label
+    limit p_limit;
 $$;
