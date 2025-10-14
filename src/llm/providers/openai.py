18bce473c8e7dad251798e8b378fbd7dbc3283e6
Roger Mähler <roger.mahler@hotmail.com>
2025-10-14 19:37:42 +0200

    fix: refactor OpenAIProvider class and improve complete method implementation

--- src/llm/providers/openai.py
+++ src/llm/providers/openai.py
@@ -1,35 +1,45 @@
+import json
+from openai import AsyncOpenAI
+from openai.types.chat.chat_completion import ChatCompletion  # type: ignore
+
+from . import Providers
+from src.configuration import ConfigValue
+
+from .provider import LLMProvider
+
 # try:
-
-#     from openai import AsyncOpenAI  # type: ignore
-
 # except ImportError:
-
 #     class AsyncOpenAI:
 #         def __init__(self, *args, **kwargs):
 #             raise ImportError("openai package not installed, cannot use OpenAIProvider")
 
 
-# from src.configuration import ConfigValue
+@Providers.register(key="openai")
+class OpenAIProvider(LLMProvider):
+    """OpenAI API provider"""
 
-# from . import Providers
-# from .provider import LLMProvider
+    def __init__(self, api_key: str = None, model: str = None):
 
+        api_key: str = api_key or ConfigValue(f"llm.{self.key}.api_key").resolve()
+        self.model: str = model or ConfigValue(f"llm.{self.key}.model").resolve()
 
-# # @Providers.register(key="openai")
-# class OpenAIProvider(LLMProvider):
-#     """OpenAI API provider"""
+        self.client = AsyncOpenAI(api_key=api_key)
 
-#     def __init__(self, api_key: str, model: str = None):
-#         self.client = AsyncOpenAI(api_key=api_key)
-#         self.model: str = model or ConfigValue("llm.openai.model").resolve()
+    async def complete(self, prompt: str, roles: dict[str, str] = None, **kwargs) -> str:
+        opts: str = kwargs.get("options", {})
+        opts = opts | (ConfigValue(f"llm.{self.key}.options").resolve() or {})
+        user_message: dict[str, str] = {
+            "role": "user",
+            "content": prompt,
+        }
+        messages: list[dict[str, str]] = (roles or []) + [
+            {
+                "role": "user",
+                "content": prompt,
+            },
+        ]
+        response: ChatCompletion = await self.client.chat.completions.create(model=self.model, messages=messages, **opts)
+        return response.choices[0].message.content.lstrip("```json").rstrip("```")
 
-#     async def complete(self, prompt: str, **kwargs) -> str:
-#         max_tokens: int = kwargs.get("max_tokens", ConfigValue("llm.openai.options.max_tokens").resolve())
-#         temperature: float = kwargs.get("temperature", ConfigValue("llm.openai.options.temperature").resolve())
-#         response = await self.client.chat.completions.create(
-#             model=self.model,
-#             messages=[{"role": "user", "content": prompt}],
-#             max_tokens=max_tokens,
-#             temperature=temperature,
-#         )
-#         return response.choices[0].message.content
+    def get_options_keys(self) -> list[str]:
+        return [("temperature", 0.1), ("max_tokens", 4048), ("stream", False)]
