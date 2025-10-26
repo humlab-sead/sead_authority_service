4083c4d34ca0bd6d4b853b0c80159dc91a8ae6eb
Roger Mähler <roger.mahler@hotmail.com>
2025-10-26 19:45:46 +0100

    feat: remove authority schema and related extensions from installation script

--- sql/install.sql
+++
@@ -1,6 +0,0 @@
-create extension if not exists unaccent;
-create extension if not exists pg_trgm;
-create extension if not exists postgis;
-create extension if not exists vector; -- apt install -y postgresql-16-pgvector
-
-select version();
