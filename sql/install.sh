31f7590f8952743e7d6326ba7921f4f332543ad5
Roger Mähler <roger.mahler@hotmail.com>
2025-11-18 13:27:35 +0100

    feat(install): enhance installation script with informative logging

--- sql/install.sh
+++ sql/install.sh
@@ -14,4 +14,8 @@ for sql_file in $(ls sql/[0-5][0-9]_*.sql | sort); do
 done
 echo "commit;" >> sql/temp_install_all.sql
 
+echo "info: Installation script generated at sql/temp_install_all.sql"
+
 /home/roger/bin/sql -q -t -A -f sql/temp_install_all.sql
+
+echo "info: Installation complete."
