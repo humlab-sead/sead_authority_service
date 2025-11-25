29bf92cd39d48e561c82005bd0369d817818c614
Roger Mähler <roger.mahler@hotmail.com>
2025-11-25 15:45:44 +0100

    refactor: streamline foreign key configuration by removing unnecessary options nesting

--- tests/arbodat/test_config_model.py
+++ tests/arbodat/test_config_model.py
@@ -137,7 +137,7 @@ class TestTableConfig:
             "site": {
                 "surrogate_id": "site_id",
                 "columns": ["site_name"],
-                "options": {"foreign_keys": [{"entity": "location", "local_keys": ["location_id"], "remote_keys": ["location_id"]}]},
+                "foreign_keys": [{"entity": "location", "local_keys": ["location_id"], "remote_keys": ["location_id"]}],
             },
             "location": {"surrogate_id": "location_id", "columns": ["location_name"]},
         }
@@ -213,12 +213,10 @@ class TestTableConfig:
             "site": {
                 "surrogate_id": "site_id",
                 "columns": ["site_name"],
-                "options": {
-                    "foreign_keys": [
-                        {"entity": "location", "local_keys": ["location_id", "location_type"], "remote_keys": ["location_id", "location_type"]},
-                        {"entity": "region", "local_keys": ["region_id"], "remote_keys": ["region_id"]},
-                    ]
-                },
+                "foreign_keys": [
+                    {"entity": "location", "local_keys": ["location_id", "location_type"], "remote_keys": ["location_id", "location_type"]},
+                    {"entity": "region", "local_keys": ["region_id"], "remote_keys": ["region_id"]},
+                ]
             },
             "location": {"surrogate_id": "location_id"},
             "region": {"surrogate_id": "region_id"},
@@ -239,9 +237,7 @@ class TestTableConfig:
                 "surrogate_id": "site_id",
                 "keys": ["site_name"],
                 "columns": ["site_name", "description", "location_id"],
-                "options": {
-                    "foreign_keys": [{"entity": "location", "local_keys": ["location_id", "location_type"], "remote_keys": ["location_id", "location_type"]}]
-                },
+                "foreign_keys": [{"entity": "location", "local_keys": ["location_id", "location_type"], "remote_keys": ["location_id", "location_type"]}]
             },
             "location": {"surrogate_id": "location_id"},
         }
@@ -260,7 +256,7 @@ class TestTableConfig:
                 "surrogate_id": "site_id",
                 "keys": ["site_name"],
                 "columns": ["site_name", "description"],
-                "options": {"foreign_keys": [{"entity": "location", "local_keys": ["location_id"], "remote_keys": ["location_id"]}]},
+                "foreign_keys": [{"entity": "location", "local_keys": ["location_id"], "remote_keys": ["location_id"]}]
             },
             "location": {"surrogate_id": "location_id"},
         }
@@ -355,8 +351,8 @@ class TestTablesConfig:
                 "columns": ["ProjektNr", "Fustel", "EVNr"],
                 "options": {
                     "drop_duplicates": ["ProjektNr", "Fustel"],
-                    "foreign_keys": [{"entity": "natural_region", "local_keys": ["NaturE"], "remote_keys": ["NaturE"]}],
                 },
+                "foreign_keys": [{"entity": "natural_region", "local_keys": ["NaturE"], "remote_keys": ["NaturE"]}],
                 "depends_on": ["natural_region"],
             },
             "natural_region": {"surrogate_id": "natural_region_id", "columns": ["NaturE", "NaturrEinh"], "options": {"drop_duplicates": True}},
@@ -395,10 +391,10 @@ class TestIntegration:
                 "columns": ["ProjektNr", "Fustel", "EVNr"],
                 "options": {
                     "drop_duplicates": ["ProjektNr", "Fustel"],
-                    "foreign_keys": [
-                        {"entity": "location", "local_keys": ["location_type", "location_name"], "remote_keys": ["location_type", "location_name"]}
-                    ],
                 },
+                "foreign_keys": [
+                    {"entity": "location", "local_keys": ["location_type", "location_name"], "remote_keys": ["location_type", "location_name"]}
+                ],
                 "depends_on": ["location"],
             },
         }
