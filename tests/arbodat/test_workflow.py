d3272250dd774fb2db1fc007621c85daa37ad5cd
Roger Mähler <roger.mahler@hotmail.com>
2025-12-03 19:28:54 +0100

    refactor(tests): improve setup_config_store calls in test workflows

--- tests/arbodat/test_workflow.py
+++ tests/arbodat/test_workflow.py
@@ -5,27 +5,37 @@ import shutil
 from src.arbodat.survey2excel import workflow
 from src.configuration.setup import setup_config_store
 
+
 # def test_workflow():
 
 #     config_file: str = "src/arbodat/input/arbodat.yml"
 #     translate: bool = False
 
 #     output_filename: str = f"output{'' if not translate else '_translated'}.xlsx"
-#     asyncio.run(setup_config_store(config_file))
+#     asyncio.run(
+#         setup_config_store(
+#             config_file,
+#             env_prefix="SEAD_NORMALIZER",
+#             env_filename="src/arbodat/input/.env",
+#             db_opts_path=None,
+#         )
+#     )
 #     asyncio.sleep(0.1)  # type: ignore ; ensure config is fully loaded;
 #     if os.path.exists(output_filename):
 #         os.remove(output_filename)
 
 #     assert not os.path.exists(output_filename)
-#     asyncio.run(workflow(
-#         input_csv="src/arbodat/input/arbodat_mal_elena_input.csv",
-#         target=output_filename,
-#         sep=";",
-#         verbose=False,
-#         translate=translate,
-#         mode="xlsx",
-#         drop_foreign_keys=False,
-#     ))
+#     asyncio.run(
+#         workflow(
+#             input_csv="src/arbodat/input/arbodat_mal_elena_input.csv",
+#             target=output_filename,
+#             sep=";",
+#             verbose=False,
+#             translate=translate,
+#             mode="xlsx",
+#             drop_foreign_keys=False,
+#         )
+#     )
 
 #     assert os.path.exists(output_filename)
 
@@ -36,7 +46,14 @@ def test_csv_workflow():
     translate: bool = False
 
     output_path: str = "tmp/arbodat/"
-    asyncio.run(setup_config_store(config_file, env_prefix="SEAD_NORMALIZER", env_filename="src/arbodat/input/.env", db_opts_path="options:database"))
+    asyncio.run(
+        setup_config_store(
+            config_file,
+            env_prefix="SEAD_NORMALIZER",
+            env_filename="src/arbodat/input/.env",
+            db_opts_path=None,
+        )
+    )
     asyncio.sleep(0.1)  # type: ignore ; ensure config is fully loaded;
 
     if os.path.exists(output_path):
