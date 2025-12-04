312e1925ea1a0ca5fed6a298339b1bed643bc378
Roger Mähler <roger.mahler@hotmail.com>
2025-12-04 12:48:21 +0100

    refactor(tests): remove commented-out test_workflow function for clarity

--- tests/arbodat/test_workflow.py
+++ tests/arbodat/test_workflow.py
@@ -5,7 +5,6 @@ import shutil
 from src.arbodat.survey2excel import workflow
 from src.configuration.setup import setup_config_store
 
-
 # def test_workflow():
 
 #     config_file: str = "src/arbodat/input/arbodat.yml"
